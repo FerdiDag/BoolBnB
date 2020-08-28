bf545b196c87888c9334a24f5468f94ced91ed45
Ferdinando <ferdi.dagostino@gmail.com>
2020-08-28 15:37:58 +0200

    installazione jquery-validation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,31 @@
 require('./bootstrap');
 var $ = require('jquery');
+require('jquery-validation');
+require('jquery-validation/dist/additional-methods.js');
+require('jquery-validation/dist/localization/messages_it.js');
 import 'bootstrap';
+
+
+
+$('form').validate({ // initialize the plugin
+    rules: {
+
+        email: {
+            required: true,
+            email: true
+        },
+
+        password: {
+            required: true,
+        },
+
+        name: {
+            lettersonly: true
+        },
+
+        lastname: {
+            lettersonly: true
+        },
+
+    }
+});
