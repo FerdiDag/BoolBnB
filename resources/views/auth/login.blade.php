9d99f9e1411e64d70df4c945d8abf329ac2ab90e
Ferdinando <ferdi.dagostino@gmail.com>
2020-08-31 13:58:46 +0200

    aggiunta navbar di bootstrap

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,9 +17,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -30,9 +30,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,59 +1,114 @@
-<div class="container">
+<nav class="navbar  navbar-expand-lg navbar-light bg-light mb-5 container">
+    <a class="navbar-brand" href="">
+        <img src="{{ asset('img/airbnb-logo.png') }}" alt="Boolean logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <!-- Authentication Links -->
+            @guest
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ "Registrati" }}</a>
+            </li>
+
+            @endif
+            @else
+            <li class="nav-item">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name ?? Auth::user()->email }}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        </ul>
+        @endguest
+
+        </ul>
+    </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+{{-- <div class="container">
     <header id="#header">
         <div class="header-left">
             <img src="{{ asset('img/airbnb-logo.png') }}" alt="">
-        </div>
-        <div class="header-center">
-            <ul>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Alloggi</a>
-                </li>
+</div>
+<div class="header-center">
+    <ul>
+        <li class="nav-item">
+            <a class="nav-link" href="">Alloggi</a>
+        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="">Esperienze</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Esperienze online</a>
-                </li>
-            </ul>
-        </div>
-        <div class="header-right">
-            <ul class="">
-                <!-- Authentication Links -->
-                @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                </li>
-                @if (Route::has('register'))
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">{{ "Registrati" }}</a>
-                </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">Esperienze</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">Esperienze online</a>
+        </li>
+    </ul>
+</div>
+<div class="header-right">
+    <ul class="">
+        <!-- Authentication Links -->
+        @guest
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+        @if (Route::has('register'))
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">{{ "Registrati" }}</a>
+        </li>
 
-                @endif
-                @else
-                <li class="nav-item">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name ?? Auth::user()->email }}
-                    </a>
+        @endif
+        @else
+        <li class="nav-item">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name ?? Auth::user()->email }}
+            </a>
 
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
+                    {{ __('Logout') }}
+                </a>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-            </ul>
-            @endguest
-        </div>
-    </header>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+        </li>
+    </ul>
+    @endguest
+</div>
+</header>
 
 </div>
-
+--}}
 
 
 
