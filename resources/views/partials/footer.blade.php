af1adf2439986a5bee4b340fe6a41cdf485a5914
Ferdinando <ferdi.dagostino@gmail.com>
2020-08-31 15:25:02 +0200

    margin-top footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="container">
-        <div class="row">
+        <div class="row mt-5">
             <div class=" col-md-3 col-sm-6">
                 <h5 class="h6 text-center">Informazioni</h5>
                 <ul class="list-unstyled text-small text-center">
