b77e49d7393d0d52e11a2a381331749319250254
Ferdinando <ferdi.dagostino@gmail.com>
2020-08-31 15:20:29 +0200

    Auto stash before rebase of "proj13_team6/master"

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,57 @@
-<div class="container">
-    <div class="row fixed-bottom">
-        <div class="col">
-            <p class="small text-center">2020 BoolBnB</p>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class=" col-md-3 col-sm-6">
+                <h5 class="h6 text-center">Informazioni</h5>
+                <ul class="list-unstyled text-small text-center">
+                    <li><a class="text-muted" href="#">Come funziona BoolBnB</a></li>
+                    <li><a class="text-muted" href="#">Newsroom</a></li>
+                    <li><a class="text-muted" href="#">Airbnb Plus</a></li>
+                    <li><a class="text-muted" href="#">Airbnb Luxe</a></li>
+                    <li><a class="text-muted" href="#">HotelTonight</a></li>
+                    <li><a class="text-muted" href="#">Airbnb for Work</a></li>
+                    <li><a class="text-muted" href="#">Olimpiadi</a></li>
+                    <li><a class="text-muted" href="#">Opportunità di lavoro</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h5 class="h6 text-center">Community</h5>
+                <ul class="list-unstyled text-small text-center">
+                    <li><a class="text-muted" href="#">Diversità e appartenenza</a></li>
+                    <li><a class="text-muted" href="#">Accesibilità</a></li>
+                    <li><a class="text-muted" href="#">Airbnb Associates</a></li>
+                    <li><a class="text-muted" href="#">Alloggi per l'emergenza</a></li>
+                    <li><a class="text-muted" href="#">Invita degli amici</a></li>
+                </ul>
+            </div>
+            <div class=" col-md-3 col-sm-6">
+                <h5 class="h6 text-center">Ospita</h5>
+                <ul class="list-unstyled text-small text-center">
+                    <li><a class="text-muted" href="#">Diventa un host</a></li>
+                    <li><a class="text-muted" href="#">Offri un'Esperienza online</a></li>
+                    <li><a class="text-muted" href="#">Offri un'Esperienza</a></li>
+                    <li><a class="text-muted" href="#">Messaggio dal Ceo Brian Chesky</a></li>
+                    <li><a class="text-muted" href="#">Ospitare responsabilmente</a></li>
+                    <li><a class="text-muted" href="#">Open homes</a></li>
+                    <li><a class="text-muted" href="#">Centro risorse</a></li>
+                    <li><a class="text-muted" href="#">Community Center</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h5 class="h6 text-center">Assistenza</h5>
+                <ul class="list-unstyled text-small text-center">
+                    <li><a class="text-muted" href="#">Aggiornamenti sulla pandemia di Covid-19</a></li>
+                    <li><a class="text-muted" href="#">Centro Assistenza</a></li>
+                    <li><a class="text-muted" href="#">Opzioni di cancellazione</a></li>
+                    <li><a class="text-muted" href="#">Servizio di assistenza di quartiere</a></li>
+                    <li><a class="text-muted" href="#">Affidabilità e sicurezza</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col mt-3">
+                <p class="small text-center">2020 BoolBnB</p>
+            </div>
         </div>
     </div>
-</div>
+</footer>
