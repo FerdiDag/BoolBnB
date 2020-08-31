924b8c1e6a367004fc643b8428002b92180c8890
Ferdinando <ferdi.dagostino@gmail.com>
2020-08-31 16:47:12 +0200

    creazione crud

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ApartmentsTableSeeder::class);
-        $this->call(RatesTableSeeder::class);
-        $this->call(ServicesTableSeeder::class);
+        // $this->call(ApartmentsTableSeeder::class);
+        // $this->call(RatesTableSeeder::class);
+        // $this->call(ServicesTableSeeder::class);
     }
 }
