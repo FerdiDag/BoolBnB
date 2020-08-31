4b25c4ea3df0a39592cdb4c5a108c0b16b19b5fa
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-08-31 14:22:50 +0200

    in lavorazione register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,19 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-5">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div id="register-header" class="card-header">{{ __('Registrati') }}</div>
 
-                <div class="card-body">
+                <div id="register-card" class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ "Email*" }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email *">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -91,8 +90,8 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ "Registra" }}
+                                <button id="register-button" type="submit" class="btn btn-primary">
+                                    {{ "Registrati" }}
                                 </button>
                             </div>
                         </div>
