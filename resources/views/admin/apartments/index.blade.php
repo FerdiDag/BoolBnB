8990b57411ed7ef02c7558f374f1a02fd0b0eaff
Ferdinando <ferdi.dagostino@gmail.com>
2020-09-02 09:35:06 +0200

    in lavorazione sezione lista appartamenti lato admin

---
+++ resources/views/admin/apartments/index.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.dashboard')
+@section('content')
+<h1 class="text-center">Lista appartamenti</h1>
+<div class="container" id="index">
+    <div class="box">
+        <img src="https://balivacationhomes.com/wp-content/uploads/2020/02/villa-zoubi-bali-vacation-homes-resize-1-23.jpg" class="rounded" alt="">
+        <h5 class="title text-center">Apartamento sito in</h5>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="features">
+            <h5 class="info">Numero di stanze: </h5>
+            <h5 class="info">Numero di letti: </h5>
+            <h5 class="info">Numero di bagni: </h5>
+            <h5 class="info">Superficie: </h5>
+            <h5 class="info">Indirizzo: </h5>
+            <h5 class="info">Prezzo per notte: </h5>
+        </div>
+        <button type="button" class="btn btn-success">Dettagli</button>
+        <button type="button" class="btn btn-primary">Modifica</button>
+        <button type="button" class="btn btn-secondary">Nascondi</button>
+        <button type="button" class="btn btn-danger">Elimina</button>
+    </div>
+    <div class="box">
+        <img src="https://balivacationhomes.com/wp-content/uploads/2020/02/villa-zoubi-bali-vacation-homes-resize-1-23.jpg" class="rounded" alt="">
+        <h5 class="title text-center">Apartamento sito in</h5>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="features">
+            <h5 class="info">Numero di stanze: </h5>
+            <h5 class="info">Numero di letti: </h5>
+            <h5 class="info">Numero di bagni: </h5>
+            <h5 class="info">Superficie: </h5>
+            <h5 class="info">Indirizzo: </h5>
+            <h5 class="info">Prezzo per notte: </h5>
+        </div>
+        <button type="button" class="btn btn-success">Dettagli</button>
+        <button type="button" class="btn btn-primary">Modifica</button>
+        <button type="button" class="btn btn-secondary">Nascondi</button>
+        <button type="button" class="btn btn-danger">Elimina</button>
+    </div>
+    <div class="box">
+        <img src="https://balivacationhomes.com/wp-content/uploads/2020/02/villa-zoubi-bali-vacation-homes-resize-1-23.jpg" class="rounded" alt="">
+        <h5 class="title text-center">Apartamento sito in</h5>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="features">
+            <h5 class="info">Numero di stanze: </h5>
+            <h5 class="info">Numero di letti: </h5>
+            <h5 class="info">Numero di bagni: </h5>
+            <h5 class="info">Superficie: </h5>
+            <h5 class="info">Indirizzo: </h5>
+            <h5 class="info">Prezzo per notte: </h5>
+        </div>
+        <button type="button" class="btn btn-success">Dettagli</button>
+        <button type="button" class="btn btn-primary">Modifica</button>
+        <button type="button" class="btn btn-secondary">Nascondi</button>
+        <button type="button" class="btn btn-danger">Elimina</button>
+    </div>
+    <div class="box">
+        <img src="https://balivacationhomes.com/wp-content/uploads/2020/02/villa-zoubi-bali-vacation-homes-resize-1-23.jpg" class="rounded" alt="">
+        <h5 class="title text-center">Apartamento sito in</h5>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="features">
+            <h5 class="info">Numero di stanze: </h5>
+            <h5 class="info">Numero di letti: </h5>
+            <h5 class="info">Numero di bagni: </h5>
+            <h5 class="info">Superficie: </h5>
+            <h5 class="info">Indirizzo: </h5>
+            <h5 class="info">Prezzo per notte: </h5>
+        </div>
+        <button type="button" class="btn btn-success">Dettagli</button>
+        <button type="button" class="btn btn-primary">Modifica</button>
+        <button type="button" class="btn btn-secondary">Nascondi</button>
+        <button type="button" class="btn btn-danger">Elimina</button>
+    </div>
+    <div class="box">
+        <img src="https://balivacationhomes.com/wp-content/uploads/2020/02/villa-zoubi-bali-vacation-homes-resize-1-23.jpg" class="rounded" alt="">
+        <h5 class="title text-center">Apartamento sito in</h5>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="features">
+            <h5 class="info">Numero di stanze: </h5>
+            <h5 class="info">Numero di letti: </h5>
+            <h5 class="info">Numero di bagni: </h5>
+            <h5 class="info">Superficie: </h5>
+            <h5 class="info">Indirizzo: </h5>
+            <h5 class="info">Prezzo per notte: </h5>
+        </div>
+        <button type="button" class="btn btn-success">Dettagli</button>
+        <button type="button" class="btn btn-primary">Modifica</button>
+        <button type="button" class="btn btn-secondary">Nascondi</button>
+        <button type="button" class="btn btn-danger">Elimina</button>
+    </div>
+    <div class="box">
+        <img src="https://balivacationhomes.com/wp-content/uploads/2020/02/villa-zoubi-bali-vacation-homes-resize-1-23.jpg" class="rounded" alt="">
+        <h5 class="title text-center">Apartamento sito in</h5>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="features">
+            <h5 class="info">Numero di stanze: </h5>
+            <h5 class="info">Numero di letti: </h5>
+            <h5 class="info">Numero di bagni: </h5>
+            <h5 class="info">Superficie: </h5>
+            <h5 class="info">Indirizzo: </h5>
+            <h5 class="info">Prezzo per notte: </h5>
+        </div>
+        <button type="button" class="btn btn-success">Dettagli</button>
+        <button type="button" class="btn btn-primary">Modifica</button>
+        <button type="button" class="btn btn-secondary">Nascondi</button>
+        <button type="button" class="btn btn-danger">Elimina</button>
+    </div>
+
+</div>
+
+@endsection
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,96 +1,98 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('page-title')</title>
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body id="bk-office" class="clearfix">
-        <header class="float-left">
-            <div class="container-fluid d-flex justify-content-between align-items-center">
-                <div id="header-left">
-                    <div class="logo-container">
-                        <img src="{{ asset('img/logo.png') }}" alt="Logo" class="img-fluid">
-                    </div>
-                </div>
-                <div id="header-right" class="d-flex justify-content-end align-items-center">
-                    <nav id="main-nav">
-                        <ul class="list-inline d-flex justify-content-end align-items-center">
-                            <li>
-                                <a href="{{ route('home') }}">
-                                    <i class="fas fa-home"></i>
-                                    <span class="d-none d-md-inline">
-                                        Home utenti
-                                    </span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('logout') }}" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                                    <i class="fas fa-sign-out-alt"></i>
-                                    <span class="d-none d-md-inline">
-                                        Logout
-                                    </span>
-                                </a>
-                            </li>
-                            <li id="aside-toggle" class="d-md-none">
-                                <i class="fas fa-bars"></i>
-                            </li>
-                        </ul>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </nav>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('page-title')</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body id="bk-office" class="clearfix">
+    <header class="float-left">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+            <div id="header-left">
+                <div class="logo-container">
+                    <img src="{{ asset('img/logo.png') }}" alt="Logo" class="img-fluid">
                 </div>
             </div>
-        </header>
-            <aside class="float-md-left">
-                <nav id="aside-nav">
-                    <ul>
+            <div id="header-right" class="d-flex justify-content-end align-items-center">
+                <nav id="main-nav">
+                    <ul class="list-inline d-flex justify-content-end align-items-center">
                         <li>
-                            <a href="#">
+                            <a href="{{ route('home') }}">
                                 <i class="fas fa-home"></i>
-                                Home admin
+                                <span class="d-none d-md-inline">
+                                    Home utenti
+                                </span>
                             </a>
                         </li>
                         <li>
-                            <a href="#">
-                                <i class="fas fa-plus"></i>
-                                Aggiungi appartamento
+                            <a href="{{ route('logout') }}" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                                <i class="fas fa-sign-out-alt"></i>
+                                <span class="d-none d-md-inline">
+                                    Logout
+                                </span>
                             </a>
                         </li>
-                        <li>
-                            <a href="#">
-                                <i class="fas fa-sliders-h"></i>
-                                Gestisci appartamenti
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i class="fas fa-envelope-open-text"></i>
-                                Messaggi
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i class="fas fa-chart-line"></i>
-                                Statistiche
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i class="fas fa-angle-double-up"></i>
-                                Sponsorizza
-                            </a>
+                        <li id="aside-toggle" class="d-md-none">
+                            <i class="fas fa-bars"></i>
                         </li>
                     </ul>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </nav>
-            </aside>
-        <main class="float-left">
-            @yield('content')
-        </main>
-    </body>
+            </div>
+        </div>
+    </header>
+    <aside class="float-md-left">
+        <nav id="aside-nav">
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-home"></i>
+                        Home admin
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-plus"></i>
+                        Aggiungi appartamento
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('admin.apartments.index')}}">
+                        <i class="fas fa-sliders-h"></i>
+                        Gestisci appartamenti
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-envelope-open-text"></i>
+                        Messaggi
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-chart-line"></i>
+                        Statistiche
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-angle-double-up"></i>
+                        Sponsorizza
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </aside>
+    <main class="float-left">
+        @yield('content')
+    </main>
+</body>
+
 </html>
