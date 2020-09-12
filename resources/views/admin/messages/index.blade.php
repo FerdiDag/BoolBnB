4ea51a2a52a747ef39213d4073e63ef3e7f9bf20
Paolo <paolo.suero@gmail.com>
2020-09-12 16:35:32 +0200

    Migliorato incartellamento view

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -13,14 +13,16 @@
             @forelse ($messages as $message)
                 <a href="{{route('admin.show_message', ['message' => $message->id])}}" class="col-12 box {{$message->status == "unread" ? "unread" : ""}}">
                     <div class="row">
-                        <div class="text-container col-12 flex-column justify-content-between">
+                        <div class="text-container d-flex align col-12 flex-column justify-content-between">
                             <div class="features">
-                                <ul>
+                                <ul class="mb-0">
                                     <li class="guest">Ricevuto da: <span>{{$message->email}}</span></li>
                                     <li>Data: <span>{{date('d/m/Y H:i', strtotime($message->created_at))}}</span></li>
-                                    <li>Appartamento: <span>{{$message->apartment->description_title}}</span></li>
                                 </ul>
                             </div>
+                        </div>
+                        <div class="col-12">
+                            <h2 class="font-weight-bold">Appartamento: <span class="text-uppercase font-weight-normal">{{$message->apartment->description_title}}</span></h2>
                         </div>
                     </div>
                 </a>
